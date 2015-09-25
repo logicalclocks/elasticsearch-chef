dc3702c49d74539725c0c58881f8d3206171b0e0
Jim Dowling <jdowling@sics.se>
2015-09-26 00:29:49 +0200

    simplify

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,33 +6,36 @@ my_ip = my_private_ip()
 
 elastic_ip = private_recipe_ip("elastic","default")
 
-elasticsearch_configure 'my_elasticsearch' do
-  user node[:elastic][:user]
-  group node[:elastic][:group]
-  dir node[:elastic][:home_dir]
-  path_conf node[:elastic][:home_dir] + "/etc/elasticsearch"
-  path_data node[:elastic][:home_dir] + "/var/data/elasticsearch"
-  path_logs node[:elastic][:home_dir] + "/var/log/elasticsearch"
-  logging({:"action" => 'INFO'})
-  allocated_memory '123m'
-  thread_stack_size '512k'
-  env_options '-DFOO=BAR'
-  gc_settings <<-CONFIG
-                -XX:+UseParNewGC
-                -XX:+UseConcMarkSweepGC
-                -XX:CMSInitiatingOccupancyFraction=75
-                -XX:+UseCMSInitiatingOccupancyOnly
-                -XX:+HeapDumpOnOutOfMemoryError
-                -XX:+PrintGCDetails
-              CONFIG
-  configuration ({
-    'cluster.name' => node[:elastic][:cluster_name],
-    'node.name' => node[:elastic][:node_name],
-    'network.host' =>  my_ip,
-    'http.cors.enabled' => true,
-    'http.cors.allow-origin' => "*"
-  })
-  action :manage
+# elasticsearch_configure 'my_elasticsearch' do
+#   user node[:elastic][:user]
+#   group node[:elastic][:group]
+#   dir node[:elastic][:home_dir]
+#   path_conf node[:elastic][:home_dir] + "/etc/elasticsearch"
+#   path_data node[:elastic][:home_dir] + "/var/data/elasticsearch"
+#   path_logs node[:elastic][:home_dir] + "/var/log/elasticsearch"
+#   logging({:"action" => 'INFO'})
+#   allocated_memory '123m'
+#   thread_stack_size '512k'
+#   env_options '-DFOO=BAR'
+#   gc_settings <<-CONFIG
+#                 -XX:+UseParNewGC
+#                 -XX:+UseConcMarkSweepGC
+#                 -XX:CMSInitiatingOccupancyFraction=75
+#                 -XX:+UseCMSInitiatingOccupancyOnly
+#                 -XX:+HeapDumpOnOutOfMemoryError
+#                 -XX:+PrintGCDetails
+#               CONFIG
+#   configuration ({
+#     'cluster.name' => node[:elastic][:cluster_name],
+#     'node.name' => node[:elastic][:node_name],
+#     'network.host' =>  my_ip,
+#     'http.cors.enabled' => true,
+#     'http.cors.allow-origin' => "*"
+#   })
+#   action :manage
+# end
+
+elasticsearch_configure 'elasticsearch' do
 end
 
 elasticsearch_service 'elasticsearch-hopsworks' do
