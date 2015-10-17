1a0f2d55469801a4e54b66d036554bd338df8a15
Jim Dowling <jdowling@sics.se>
2015-10-18 00:16:30 +0200

    purge recipe updated

--- recipes/purge.rb
+++ recipes/purge.rb
@@ -1,8 +1,46 @@
+
+node[:elastic][:rivers].each { |d| 
+
+  bash 'kill_running_service_#{d}' do
+    user "root"
+    ignore_failure :true
+    code <<-EOF
+      service stop #{d}
+#      pkill ??
+    EOF
+  end
+
+  file "/etc/init.d/#{d}" do
+    action :delete
+    ignore_failure :true
+  end
+}
+
+
 elasticsearch_install 'my_es_installation' do
   type :tarball
   dir '/usr/local' 
   owner node[:elastic][:user]
   group node[:elastic][:group]
   tarball_url node[:elastic][:url]
+  ignore_failure :true
   action :remove
+end
+
+directory node[:elastic][:version_dir] do
+  recursive true
+  action :delete
+  ignore_failure :true
+end
+
+link node[:elastic][:home_dir] do
+  action :delete
+  ignore_failure :true
+end
+
+
+directory "/usr/local/elasticsearch-jdbc-#{node[:elastic][:jdbc_river][:version]}" do
+  recursive true
+  action :delete
+  ignore_failure :true
 end
