b816c655e687d6a329290a4462505148c64240b5
Jim Dowling <jdowling@sics.se>
2015-10-26 14:29:20 +0100

    bug

--- recipes/install.rb
+++ recipes/install.rb
@@ -59,7 +59,7 @@ bash "locate_mysql_river" do
     code <<-EOF
    set -e
    mv /tmp/#{mysql_base} /usr/local
-   chown -R #{node[:elastic][:user]} /usr/local/#{mysql_base}
+   chown -R #{node[:elastic][:user]} #{node[:elastic][:dir]}/#{mysql_base}
    touch #{node[:elastic][:home_dir]}/.#{mysql_base}_moved
 EOF
   not_if { ::File.exists?( "#{node[:elastic][:home_dir]}/.#{mysql_base}_moved")}
