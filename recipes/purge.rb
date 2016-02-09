53c62d1669a1e62e7574e57fc9f06d8f280565de
%github_username% <jdowling@sics.se>
2016-02-09 08:18:40 +0100

    fixing purge list

--- recipes/purge.rb
+++ recipes/purge.rb
@@ -1,12 +1,21 @@
 
+case node.platform
+when "ubuntu"
+ if node.platform_version.to_f <= 14.04
+   node.override.elastic.systemd = "false"
+ end
+end
+
+if node.elastic.systemd == "false"
+
+
 node[:elastic][:rivers].each { |d| 
 
   bash 'kill_running_service_#{d}' do
     user "root"
     ignore_failure :true
     code <<-EOF
-      service stop #{d}
-#      pkill ??
+      service #{d} stop
     EOF
   end
 
@@ -17,15 +26,63 @@ node[:elastic][:rivers].each { |d|
 }
 
 
-elasticsearch_install 'my_es_installation' do
-  type :tarball
-  dir "#{node[:elastic][:dir]}"
-  owner node[:elastic][:user]
-  group node[:elastic][:group]
-  tarball_url node[:elastic][:url]
-  ignore_failure :true
-  action :remove
+  bash 'kill_running_elastic_service' do
+    user "root"
+    ignore_failure :true
+    code <<-EOF
+      service elasticsearch-#{node[:elastic][:node_name]} stop
+      pkill elasticsearch-#{node[:elastic][:node_name]}
+      rm /etc/init.d/elasticsearch-#{node[:elastic][:node_name]}
+    EOF
+  end
+
+
+else # systemd
+
+
+node[:elastic][:rivers].each { |d| 
+
+  bash 'kill_running_service_#{d}' do
+    user "root"
+    ignore_failure :true
+    code <<-EOF
+      systemctl stop #{d}
+    EOF
+  end
+
+  file "/usr/lib/systemd/system/#{d}.service" do
+    action :delete
+    ignore_failure :true
+  end
+  file "/lib/systemd/system/#{d}.service" do
+    action :delete
+    ignore_failure :true
+  end
+}
+
+
+  bash 'kill_running_elastic_service' do
+    user "root"
+    ignore_failure :true
+    code <<-EOF
+      systemctl stop elasticsearch-#{node[:elastic][:node_name]}
+      pkill elasticsearch-#{node[:elastic][:node_name]}
+      rm -f /usr/lib/systemd/system/elasticsearch-#{node[:elastic][:node_name]}
+      rm -f /lib/systemd/system/elasticsearch-#{node[:elastic][:node_name]}
+    EOF
+  end
+
 end
+
+# elasticsearch_install 'my_es_installation' do
+#   type :tarball
+#   dir "#{node[:elastic][:dir]}"
+#   owner node[:elastic][:user]
+#   group node[:elastic][:group]
+#   tarball_url node[:elastic][:url]
+#   ignore_failure :true
+#   action :remove
+# end
 
 directory node[:elastic][:version_dir] do
   recursive true
