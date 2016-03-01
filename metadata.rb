fa86a9ae0efecc0128a4da20fda94e72c2938145
Jim Dowling <jdowling@sics.se>
2016-03-01 21:32:38 +0100

    release script for supermarket

--- metadata.rb
+++ metadata.rb
@@ -3,15 +3,13 @@ maintainer       "Jim Dowling"
 maintainer_email "jdowling@kth.se"
 license          "Apache v2.0"
 description      'Installs/Configures/Runs elasticsearch'
-version          "0.1"
+version          "0.1.0"
 
 recipe            "elastic::install", "Experiment setup for elasticsearch"
 recipe            "elastic::default",  "Configures and starts an elasticsearch server"
 
 
 depends "kagent"
-#depends 'apt'
-#depends 'chef-sugar'
 depends "java"
 depends "elasticsearch"
 depends "ulimit"
