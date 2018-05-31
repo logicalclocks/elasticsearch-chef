cdcf5c23d74edb76fa934e038005520eaa7138ba
robzor92 <robin.eric.andersson@gmail.com>
2018-05-31 10:41:42 +0200

    create templates

--- providers/start.rb
+++ providers/start.rb
@@ -31,7 +31,7 @@ Chef::Log.info  "Elastic Ip is: http://#{new_resource.elastic_ip}:9200"
 
 indexes_installed = "#{node['elastic']['home_dir']}/.indexes_installed"
 
- http_request 'elastic-install-indexes' do
+ http_request 'elastic-install-projects-index' do
    url "http://#{new_resource.elastic_ip}:9200/projects"
    headers 'Content-Type' => 'application/json'
    message '
@@ -93,6 +93,80 @@ indexes_installed = "#{node['elastic']['home_dir']}/.indexes_installed"
    not_if { ::File.exists?( indexes_installed ) }       
  end
 
+ http_request 'elastic-create-logs-template' do
+   url "http://#{new_resource.elastic_ip}:9200/_template/logs"
+   headers 'Content-Type' => 'application/json'
+   message '
+   {
+     "template":"*_logs",
+     "mappings":{
+       "logs":{
+         "properties":{
+           "application":{
+             "type":"keyword"
+           },
+           "host":{
+             "type":"keyword"
+           },
+           "jobname":{
+             "type":"keyword"
+           },
+           "file":{
+             "type":"keyword"
+           },
+           "timestamp":{
+             "type":"date"
+           },
+           "project":{
+             "type":"keyword"
+           }
+         }
+       }
+     }
+   }'
+   action :put
+   retries numRetries
+   retry_delay retryDelay
+   not_if { ::File.exists?( indexes_installed ) }
+ end
+
+ http_request 'elastic-create-experiments-template' do
+   url "http://#{new_resource.elastic_ip}:9200/_template/experiments"
+   headers 'Content-Type' => 'application/json'
+   message '
+   {
+     "template":"*_experiments",
+     "mappings":{
+       "experiments":{
+         "properties":{
+           "hops-py-library":{
+             "type":"keyword"
+           },
+           "tensorflow":{
+             "type":"keyword"
+           },
+           "spark":{
+             "type":"keyword"
+           },
+           "hopsworks":{
+             "type":"keyword"
+           },
+           "hops":{
+             "type":"keyword"
+           },
+           "kafka":{
+             "type":"keyword"
+           }
+         }
+       }
+     }
+   }'
+   action :put
+   retries numRetries
+   retry_delay retryDelay
+   not_if { ::File.exists?( indexes_installed ) }
+ end
+
   bash 'elastic-indexes-installed' do
      user node['elastic']['user']
     code <<-EOF
@@ -100,7 +174,6 @@ indexes_installed = "#{node['elastic']['home_dir']}/.indexes_installed"
         touch #{indexes_installed}
   EOF
   end
-
  
 #  new_resource.updated_by_last_action(false)
 end
