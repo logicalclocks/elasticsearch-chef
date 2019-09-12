16726996b456b5bd99d2876fc06a22bdf41762ac
Fabio Buso <buso.fabio@gmail.com>
2019-09-12 10:22:09 +0200

    do not create base dir

--- recipes/install.rb
+++ recipes/install.rb
@@ -2,11 +2,3 @@
 sysctl_param 'vm.max_map_count' do
   value node['elastic']['kernel']['vm.max_map_count']
 end
-
-directory node['elastic']['dir'] do
-  owner node['elastic']['user']
-  group node['elastic']['group']
-  mode '0750'
-  action :create
-  not_if { ::Dir.exists?(node['elastic']['dir']) }
-end
