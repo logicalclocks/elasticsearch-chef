0d389cac26fc1dda5ac01359aa39c1c979115595
Alex Ormenisan <aaor@kth.se>
2020-04-17 13:13:13 +0200

    [HOPSWORKS-1667] featurestore index (#38)

--- attributes/default.rb
+++ attributes/default.rb
@@ -117,3 +117,4 @@ default['elastic']['opendistro_security']['audit']['threadpool']['max_queue_len'
 default['elastic']['epipe']['search_index']                                         = "projects"
 default['elastic']['epipe']['app_provenance_index']                                 = "app_prov"
 default['elastic']['epipe']['file_provenance_index_pattern']                        = "*__file_prov"
+default['elastic']['epipe']['featurestore_index']                                   = "featurestore"
