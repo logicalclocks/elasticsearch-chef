7c548d5d16d4f77781b62cff83f53c622f24d4e9
Theofilos Kakantousis <t.kakantousis@gmail.com>
2020-06-13 12:04:26 +0200

    hops gid

--- recipes/install.rb
+++ recipes/install.rb
@@ -2,3 +2,10 @@
 sysctl_param 'vm.max_map_count' do
   value node['elastic']['kernel']['vm.max_map_count']
 end
+
+group node['hops']['group'] do
+  gid node['hops']['group_id']
+  action :create
+  not_if "getent group #{node['hops']['group']}"
+  not_if { node['install']['external_users'].casecmp("true") == 0 }
+end
