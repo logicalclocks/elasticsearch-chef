7150787776641b4c740c92e7445e7865607e99da
Mahmoud Ismail <mahmoud@logicalclocks.com>
2020-06-24 10:03:02 +0200

    [HOPSWORKS-1871] Add admin DNs to all elasticsearch nodes (#46)

--- libraries/elastic_helpers.rb
+++ libraries/elastic_helpers.rb
@@ -14,10 +14,11 @@ module Elastic
       return hosts.map{|k, v| "CN=#{v},OU=*,L=#{node['elastic']['user']},ST=Sweden,C=SE"}
     end
 
-    def get_elastic_admin_dn()
-      return ["CN=#{node['fqdn']},OU=0,L=#{node['elastic']['elk-user']},ST=Sweden,C=SE"]
+    def get_all_elastic_admin_dns()
+      hosts = lookup_ips(all_elastic_ips())
+      return hosts.map{|k, v| "CN=#{v},OU=0,L=#{node['elastic']['elk-user']},ST=Sweden,C=SE"}
     end
-
+    
     def all_elastic_host_names()
       hosts = lookup_ips(all_elastic_ips())
       return hosts.map{|k, v| v}
