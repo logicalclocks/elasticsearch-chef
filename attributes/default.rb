c77d29f11da40d0e0edb2afafa0d39b92a83330b
Jim Dowling <jim@logicalclocks.com>
2021-10-07 20:51:47 +0200

    fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -45,7 +45,7 @@ default['elastic']['limits']['memory_limit'] = 'infinity'
 default['elastic']['systemd']               = "true"
 
 default['elastic']['memory']['Xms']         = "1024m"
-default['elastic']['memory']['Xmx']         = "1024m"
+default['elastic']['memory']['Xmx']         = "1500m"
 default['elastic']['cluster']['max_shards_per_node'] = "3000"
 
 default['elastic']['thread_stack_size']     = "512k"
