57579188050281430ca4ebf0d1a5b08eb65a670f
Jim Dowling <jim@logicalclocks.com>
2021-10-07 23:33:04 +0200

    fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -112,6 +112,8 @@ bash 'extract_elastic' do
                 chown -R #{node['elastic']['user']}:#{node['elastic']['group']} #{node['elastic']['home']}
                 chmod 750 #{node['elastic']['home']}
                 cd #{node['elastic']['home']}
+                # Java 11 reqd -  java.nio.file.Path.of(..) not in Java 8, so remove this plugin
+                mv plugins/opendistro-reports-scheduler .
                 touch #{elastic_downloaded}
                 chown #{node['elastic']['user']} #{elastic_downloaded}
         EOH
