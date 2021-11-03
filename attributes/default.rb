ff8947c800d4fffd0f6b71f72509b5107b6bc921
Jim Dowling <jim@logicalclocks.com>
2021-11-03 23:40:45 +0100

    rename to opensearch

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
 include_attribute "kagent"
 
-default['elastic']['opendistro']['version'] = "1.1.0"
-default['elastic']['version']               = node['elastic']['opendistro']['version']
+default['elastic']['opensearch']['version'] = "1.1.0"
+default['elastic']['version']               = node['elastic']['opensearch']['version']
 default['elastic']['install_type']          = "tarball"
 #default['elastic']['checksum']              = "8ba8a7c1e32e02056d054638e144290c396b9c0656806a4249ac83fcd28b3c84f89eccf437200ffa435e3edb9f362d20c0a296d70d5a6fa583fd61f21047b16b"
-default['elastic']['url']                   = node['download_url'] + "/opendistro/opensearch-#{node['elastic']['opendistro']['version']}-linux-x64.tar.gz"
+default['elastic']['url']                   = node['download_url'] + "/opensearch/opensearch-#{node['elastic']['opensearch']['version']}-linux-x64.tar.gz"
 default['elastic']['user']                  = node['install']['user'].empty? ? "elastic" : node['install']['user']
 default['elastic']['user_id']               = '1501'
 default['elastic']['elk-user']              = node['install']['user'].empty? ? "elkadmin" : node['install']['user']
@@ -30,7 +30,7 @@ default['elastic']['data_volume']['data_dir'] = "#{node['elastic']['data_volume'
 default['elastic']['data_volume']['log_dir']  = "#{node['elastic']['data_volume']['root_dir']}/logs"
 
 default['elastic']['dir']                   = node['install']['dir'].empty? ? "/usr/local" : node['install']['dir']
-default['elastic']['home']           = "#{node['elastic']['dir']}/opensearch-#{node['elastic']['opendistro']['version']}"
+default['elastic']['home']           = "#{node['elastic']['dir']}/opensearch-#{node['elastic']['opensearch']['version']}"
 default['elastic']['base_dir']              = "#{node['elastic']['dir']}/opensearch"
 default['elastic']['data_dir']              = "#{node['elastic']['dir']}/opensearch-data"
 default['elastic']['config_dir']            = "#{node['elastic']['base_dir']}/config"
@@ -76,56 +76,56 @@ default['elastic']['exporter']['flags']         = %w[--es.all
     --es.shards
 ]
 
-#OpenDistro Security Plugin
-default['elastic']['opendistro_security']['enabled']                                = "true"
-default['elastic']['opendistro_security']['url']                                    = "#{node['download_url']}/opendistro_security-#{node['elastic']['opendistro']['version']}.zip"
-default['elastic']['opendistro_security']['base_dir']                               = "#{node['elastic']['plugins_dir']}/opendistro_security"
-default['elastic']['opendistro_security']['config_dir']                             = "#{node['elastic']['opendistro_security']['base_dir']}/securityconfig"
-default['elastic']['opendistro_security']['tools_dir']                              = "#{node['elastic']['opendistro_security']['base_dir']}/tools"
-default['elastic']['opendistro_security']['tools']['hash']                          = "#{node['elastic']['opendistro_security']['tools_dir']}/hash.sh"
-default['elastic']['opendistro_security']['tools']['securityadmin']                 = "#{node['elastic']['opendistro_security']['tools_dir']}/securityadmin.sh"
+#Opensearch Security Plugin
+default['elastic']['opensearch_security']['enabled']                                = "true"
+#default['elastic']['opensearch_security']['url']                                    = "#{node['download_url']}/opensearch_security-#{node['elastic']['opensearch']['version']}.zip"
+default['elastic']['opensearch_security']['base_dir']                               = "#{node['elastic']['plugins_dir']}/opensearch-security"
+default['elastic']['opensearch_security']['config_dir']                             = "#{node['elastic']['opensearch_security']['base_dir']}/securityconfig"
+default['elastic']['opensearch_security']['tools_dir']                              = "#{node['elastic']['opensearch_security']['base_dir']}/tools"
+default['elastic']['opensearch_security']['tools']['hash']                          = "#{node['elastic']['opensearch_security']['tools_dir']}/hash.sh"
+default['elastic']['opensearch_security']['tools']['securityadmin']                 = "#{node['elastic']['opensearch_security']['tools_dir']}/securityadmin.sh"
 
-default['elastic']['opendistro_security']['admin']['username']                      = "admin"
-default['elastic']['opendistro_security']['admin']['password']                      = "adminpw"
-default['elastic']['opendistro_security']['kibana']['username']                     = "kibanaserver"
-default['elastic']['opendistro_security']['kibana']['password']                     = "kibanaserver"
-default['elastic']['opendistro_security']['logstash']['username']                   = "logstash"
-default['elastic']['opendistro_security']['logstash']['password']                   = "logstash"
-default['elastic']['opendistro_security']['elastic_exporter']['username']           = "elastic_exporter"
-default['elastic']['opendistro_security']['elastic_exporter']['password']           = "elastic_exporter"
-default['elastic']['opendistro_security']['epipe']['username']                      = "epipeuser"
-default['elastic']['opendistro_security']['epipe']['password']                      = "epipepassword"
-default['elastic']['opendistro_security']['service_log_viewer']['username']         = "service_log_viewer"
-default['elastic']['opendistro_security']['service_log_viewer']['password']         = "service_log_viewer"
+default['elastic']['opensearch_security']['admin']['username']                      = "admin"
+default['elastic']['opensearch_security']['admin']['password']                      = "adminpw"
+default['elastic']['opensearch_security']['kibana']['username']                     = "kibanaserver"
+default['elastic']['opensearch_security']['kibana']['password']                     = "kibanaserver"
+default['elastic']['opensearch_security']['logstash']['username']                   = "logstash"
+default['elastic']['opensearch_security']['logstash']['password']                   = "logstash"
+default['elastic']['opensearch_security']['elastic_exporter']['username']           = "elastic_exporter"
+default['elastic']['opensearch_security']['elastic_exporter']['password']           = "elastic_exporter"
+default['elastic']['opensearch_security']['epipe']['username']                      = "epipeuser"
+default['elastic']['opensearch_security']['epipe']['password']                      = "epipepassword"
+default['elastic']['opensearch_security']['service_log_viewer']['username']         = "service_log_viewer"
+default['elastic']['opensearch_security']['service_log_viewer']['password']         = "service_log_viewer"
 
-default['elastic']['opendistro_security']['keystore']['type']                       = "JKS"
-default['elastic']['opendistro_security']['keystore']['file']                       = "kstore.jks"
-default['elastic']['opendistro_security']['keystore']['location']                   = "#{node['elastic']['config_dir']}/#{node['elastic']['opendistro_security']['keystore']['file']}"
-default['elastic']['opendistro_security']['keystore']['password']                   = node['hopsworks']['master']['password']
+default['elastic']['opensearch_security']['keystore']['type']                       = "JKS"
+default['elastic']['opensearch_security']['keystore']['file']                       = "kstore.jks"
+default['elastic']['opensearch_security']['keystore']['location']                   = "#{node['elastic']['config_dir']}/#{node['elastic']['opensearch_security']['keystore']['file']}"
+default['elastic']['opensearch_security']['keystore']['password']                   = node['hopsworks']['master']['password']
 
-default['elastic']['opendistro_security']['truststore']['type']                     = "JKS"
-default['elastic']['opendistro_security']['truststore']['file']                     = "tstore.jks"
-default['elastic']['opendistro_security']['truststore']['location']                 = "#{node['elastic']['config_dir']}/#{node['elastic']['opendistro_security']['truststore']['file']}"
-default['elastic']['opendistro_security']['truststore']['password']                 = node['hopsworks']['master']['password']
+default['elastic']['opensearch_security']['truststore']['type']                     = "JKS"
+default['elastic']['opensearch_security']['truststore']['file']                     = "tstore.jks"
+default['elastic']['opensearch_security']['truststore']['location']                 = "#{node['elastic']['config_dir']}/#{node['elastic']['opensearch_security']['truststore']['file']}"
+default['elastic']['opensearch_security']['truststore']['password']                 = node['hopsworks']['master']['password']
 
-default['elastic']['opendistro_security']['https']['enabled']                       = "true"
-default['elastic']['opendistro_security']['kibana']['multitenancy']['enabled']      = "true"
-default['elastic']['opendistro_security']['kibana']['index']                        = ".kibana"
+default['elastic']['opensearch_security']['https']['enabled']                       = "true"
+default['elastic']['opensearch_security']['kibana']['multitenancy']['enabled']      = "true"
+default['elastic']['opensearch_security']['kibana']['index']                        = ".kibana"
 
-default['elastic']['opendistro_security']['jwt']['enabled']                         = "true"
-default['elastic']['opendistro_security']['jwt']['url_parameter']                   = "jt"
-default['elastic']['opendistro_security']['jwt']['roles_key']                       = "roles"
-default['elastic']['opendistro_security']['jwt']['subject_key']                     = "sub"
-default['elastic']['opendistro_security']['jwt']['exp_ms']                          = "1800000"
+default['elastic']['opensearch_security']['jwt']['enabled']                         = "true"
+default['elastic']['opensearch_security']['jwt']['url_parameter']                   = "jt"
+default['elastic']['opensearch_security']['jwt']['roles_key']                       = "roles"
+default['elastic']['opensearch_security']['jwt']['subject_key']                     = "sub"
+default['elastic']['opensearch_security']['jwt']['exp_ms']                          = "1800000"
 
-default['elastic']['opendistro_security']['roles']['data_owner']['role_name']       = 'data_owner'
-default['elastic']['opendistro_security']['roles']['data_scientist']['role_name']   = 'data_scientist'
+default['elastic']['opensearch_security']['roles']['data_owner']['role_name']       = 'data_owner'
+default['elastic']['opensearch_security']['roles']['data_scientist']['role_name']   = 'data_scientist'
 
-default['elastic']['opendistro_security']['audit']['enable_rest']                   = "true"
-default['elastic']['opendistro_security']['audit']['enable_transport']              = "false"
-default['elastic']['opendistro_security']['audit']['type']                          = "internal_elasticsearch"
-default['elastic']['opendistro_security']['audit']['threadpool']['size']            = 10
-default['elastic']['opendistro_security']['audit']['threadpool']['max_queue_len']   = 100000
+default['elastic']['opensearch_security']['audit']['enable_rest']                   = "true"
+default['elastic']['opensearch_security']['audit']['enable_transport']              = "false"
+default['elastic']['opensearch_security']['audit']['type']                          = "internal_elasticsearch"
+default['elastic']['opensearch_security']['audit']['threadpool']['size']            = 10
+default['elastic']['opensearch_security']['audit']['threadpool']['max_queue_len']   = 100000
 
 
 default['elastic']['epipe']['search_index']                                         = "projects"
